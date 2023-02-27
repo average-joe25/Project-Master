919c78e06d621e8a4a922a54e3247bf13bbb2ab6
Abhishek Kumar Singh <abhishekkrsingh369@gmail.com>
2023-02-27 20:28:08 +0530

    bug fix 3

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import './projectList.css';
 import Avatar from './Avatar';
+import { useAuthContext } from '../hooks/useAuthContext';
+import 'font-awesome/css/font-awesome.min.css';
 export default function ProjectList({projects}) {
+    const {user}=useAuthContext();
     return (
         <div className='project-list'>
             {projects.length===0 && <p>No projects yet!</p>}
-            {projects.map(project => (
+            {projects.filter(((project)=>{
+                return (project.assignedUsersList.find((assigned) => {return assigned.id === user.id})||project.createdBy.id===user.uid); 
+            })).map(project => (
                 <Link key={project.id} to={'/projects/'+project.id}>
                     <h4>{project.name}</h4>
+                    <i class="fa fa-trash" aria-hidden="true"></i>
                     <p>due by {project.dueDate.toDate().toDateString()}</p>
                     <div className='assigned-to'>
                         <ul>
@@ -17,7 +23,6 @@ export default function ProjectList({projects}) {
                         </ul>
                     </div>
                 </Link>
-
             ))}
         </div>
     );
