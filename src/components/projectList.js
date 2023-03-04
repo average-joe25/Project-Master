093a04cdd40470ffb26aaf044d3b9886a61ea0f6
Abhishek Kumar Singh <abhishekkrsingh369@gmail.com>
2023-03-04 11:15:01 +0530

    delete button

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -3,8 +3,16 @@ import './projectList.css';
 import Avatar from './Avatar';
 import { useAuthContext } from '../hooks/useAuthContext';
 import 'font-awesome/css/font-awesome.min.css';
+import { useFirestore } from '../hooks/useFirestore';
+import { faHouseMedicalCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 export default function ProjectList({projects}) {
+    const {deleteDocument}=useFirestore('projects');
     const {user}=useAuthContext();
+    const handleDelete = (e) => {
+        e.preventDefault();
+        deleteDocument(e.target.id);
+
+    }
     return (
         <div className='project-list'>
             {projects.length===0 && <p>No projects yet!</p>}
@@ -12,8 +20,8 @@ export default function ProjectList({projects}) {
                 return (project.assignedUsersList.find((assigned) => {return assigned.id === user.uid})||project.createdBy.id===user.uid); 
             })).map(project => (
                 <Link key={project.id} to={'/projects/'+project.id}>
-                    <h4>{project.name}</h4>
-                    <i class="fa fa-trash" aria-hidden="true"></i>
+                    <div className='name-trash'><h4>{project.name}</h4>
+                    <i id={project.id} class="fa fa-trash" aria-hidden="true" onClick={handleDelete}></i></div>
                     <p>due by {project.dueDate.toDate().toDateString()}</p>
                     <div className='assigned-to'>
                         <ul>
